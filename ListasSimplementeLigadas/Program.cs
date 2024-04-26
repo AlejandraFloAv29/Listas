430799daa7ed61089b645ac7cd838664ff1d0577
Alejandra Flores <Veronica_Flores23@unikino.edu.mx>
2024-04-25 22:05:08 -0700

    Add project files.

---
+++ ListasSimplementeLigadas/Program.cs
@@ -0,0 +1,24 @@
+﻿using ListasSimplementeLigadas;
+
+Lista MiLista = new Lista();
+MiLista.Agregar(" CERO ");
+MiLista.Agregar(" UNO ");
+MiLista.Agregar(" DOS ");
+MiLista.Agregar(" TRES ");
+MiLista.Agregar(" CUATRO ");
+
+string elementos = MiLista.Recorrer();
+Console.WriteLine(elementos);
+
+Console.WriteLine();
+Nodo encontrado = MiLista.Buscar(" TRES ");
+Console.WriteLine(encontrado.Valor);
+
+Console.WriteLine();
+Nodo encontrado2 = MiLista.BuscarNodoAnterior(" DOS ");
+Console.WriteLine(encontrado2.Valor);
+
+Console.WriteLine();
+MiLista.Borrar(" DOS ");
+elementos = MiLista.Recorrer();
+Console.WriteLine(elementos);
